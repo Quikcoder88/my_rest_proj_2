de96d0a68644a14458266209317de201c3d9b725
Quikcoder <Quikcoder88@gmail.com>
2023-07-05 15:03:48 +0300

    initial commit

---
+++ first/nums.py
@@ -0,0 +1,13 @@
+# видео от Python Russian https://www.youtube.com/watch?v=9VKKZNqzPcE&t=17s
+# Упор на работе с git через ide
+
+# в файл '.gitignore' прописываем /venv/, /.idea/
+
+# выделяем файлы которые хотим сохранять, ПКМ - Git - Add (или Ctrl - Alt - A) - Добавили в отслеживание
+
+def plus(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    print(plus(2, 2))
